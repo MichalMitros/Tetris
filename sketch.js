6e0ddc3d37bd5c403499ac5cb953cb7a3e1a186a
MichalMitros <mitrosmicha4@gmail.com>
2020-04-11 19:45:08 +0200

    add basic game mechanics

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ const padding = 40;
 let size;
 let board = [];
 let tetroid = [];
+let tetroidType;
+let delay = 16;
+let tick = 0;
 
 function setup() {
   if(window.innerWidth <= window.innerHeight) {
@@ -17,10 +20,12 @@ function setup() {
   for(let i=0; i<20; i++) {
     board[i] = [];
     for(let j=0; j<10; j++) {
-      board[i][j] = Math.floor(Math.random()*8);
-      // board[i][j] = 0;
+      // board[i][j] = Math.floor(Math.random()*8);
+      board[i][j] = 0;
     }
   }
+
+  createTetroid();
 
   createCanvas(size, size);
 }
@@ -28,10 +33,102 @@ function setup() {
 function draw() {
   frameRate(30);
   background(0);
-  drawBoard(board);
+  drawBoard();
+
+  if(tick === delay) {
+    tick = 0;
+
+    if(keyIsDown(LEFT_ARROW)) {
+      if(canBeMoved(-1)) {
+        moveTetroid(-1);
+      }
+    } else if(keyIsDown(RIGHT_ARROW)) {
+      if(canBeMoved(1)) {
+        moveTetroid(1);
+      }
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+      if(canBeMoved(0)) {
+        moveTetroid(0);
+      }
+    }
+
+    if(canBeMoved(0)) {
+      moveTetroid(0);
+    } else {
+      saveTetroid();
+      removeFilledRows();
+      createTetroid();
+      if(isGameOver()) {
+        clearBoard();
+        createTetroid();
+      }
+    }
+  } else {
+    tick++;
+    if(tick === delay/2) {
+      if(keyIsDown(LEFT_ARROW)) {
+        if(canBeMoved(-1)) {
+          moveTetroid(-1);
+        }
+      } else if(keyIsDown(RIGHT_ARROW)) {
+        if(canBeMoved(1)) {
+          moveTetroid(1);
+        }
+      }
+      if(keyIsDown(DOWN_ARROW)) {
+        if(canBeMoved(0)) {
+          moveTetroid(0);
+        }
+      }
+    }
+  }
 }
 
-function drawBoard(b) {
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    if(canBeMoved(-1)) {
+      moveTetroid(-1);
+    }
+  } else if(keyCode === RIGHT_ARROW) {
+    if(canBeMoved(1)) {
+      moveTetroid(1);
+    }
+  } else if(keyCode === DOWN_ARROW) {
+    if(canBeMoved(0)) {
+      moveTetroid(0);
+    } else {
+      saveTetroid();
+      removeFilledRows();
+      createTetroid();
+      if(isGameOver()) {
+        clearBoard();
+        createTetroid();
+      }
+    }
+  }
+}
+
+function removeFilledRows() {
+  for(let i=19; i>=0; i--) {
+    let isFilled = true;
+    for(let j=0; j<10; j++) {
+      if(board[i][j] === 0) {
+        isFilled = false;
+      }
+    }
+    if(isFilled) {
+      board[i] = undefined;
+      for(let j=i; j>0; j--) {
+        board[j] = board[j-1];
+      }
+      board[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      i++;
+    }
+  }
+}
+
+function drawBoard() {
   translate(3, 3);
   strokeWeight(1);
   stroke(0);
@@ -39,7 +136,14 @@ function drawBoard(b) {
   let s = (size - 6) / 20;
   for(let i=0; i<20; i++) {
     for(let j=0; j<10; j++) {
-      switch(b[i][j]) {
+      let c = board[i][j];
+      for(let k=0; k<tetroid.length; k++) {
+        if(tetroid[k][0] === i && tetroid[k][1] === j) {
+          c = tetroidType + 1;
+          break;
+        }
+      }
+      switch(c) {
         case 1:
           // Z
           fill(87, 195, 245);
@@ -97,6 +201,145 @@ function windowResized() {
   resizeCanvas(size + 10, size + 10);
 }
 
-function createTrtroid() {
+function isGameOver() {
+  for(let i=0; i<tetroid.length; i++) {
+    if(board[tetroid[i][0]][tetroid[i][1]] > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function clearBoard() {
+  for(let i=0; i<20; i++) {
+    for(let j=0; j<10; j++) {
+      board[i][j] = 0;
+    }
+  }
+}
+
+function saveTetroid() {
+  for(let i=0; i<tetroid.length; i++) {
+    board[tetroid[i][0]][tetroid[i][1]] = tetroidType+1;
+  }
+}
+
+function moveTetroid(direction) {
+  /*
+    -1 - left
+    0 - down
+    1 - right
+  */
+
+  switch(direction) {
+    case -1:
+      for(let i=0; i<tetroid.length; i++) {
+        tetroid[i][1]--;
+      }
+      break;
+    case 0:
+      for(let i=0; i<tetroid.length; i++) {
+        tetroid[i][0]++;
+      }
+      break;
+    case 1:
+      for(let i=0; i<tetroid.length; i++) {
+        tetroid[i][1]++;
+      }
+      break;
+  }
+}
+
+function canBeMoved(direction) {
+  /*
+    -1 - left
+    0 - down
+    1 - right
+  */
+
+  switch(direction) {
+    case -1:
+      for(let i=0; i<tetroid.length; i++) {
+        if(tetroid[i][1] === 0) {
+          return false;
+        } else if(board[tetroid[i][0]][tetroid[i][1]-1] > 0) {
+          return false;
+        }
+      }
+      break;
+    case 0:
+      for(let i=0; i<tetroid.length; i++) {
+        if(tetroid[i][0] === 19) {
+          return false;
+        } else if(board[tetroid[i][0]+1][tetroid[i][1]] > 0) {
+          return false;
+        }
+      }
+      break;
+    case 1:
+      for(let i=0; i<tetroid.length; i++) {
+        if(tetroid[i][1] === 9) {
+          return false;
+        } else if(board[tetroid[i][0]][tetroid[i][1]+1] > 0) {
+          return false;
+        }
+      }
+      break;
+  }
+
+  return true;
+}
+
+function createTetroid() {
   let type = Math.floor(Math.random() * 7);
+  let result = [];
+  type = 5;
+
+  switch(type) {
+    case 0:
+      result[0] = [0, 3];
+      result[1] = [0, 4];
+      result[2] = [1, 4];
+      result[3] = [1, 5];
+      break;
+    case 1:
+      result[0] = [1, 3];
+      result[1] = [1, 4];
+      result[2] = [0, 4];
+      result[3] = [0, 5];
+      break;
+    case 2:
+      result[0] = [0, 4];
+      result[1] = [1, 4];
+      result[2] = [2, 4];
+      result[3] = [2, 5];
+      break;
+    case 3:
+      result[0] = [0, 5];
+      result[1] = [1, 5];
+      result[2] = [2, 5];
+      result[3] = [2, 4];
+      break;
+    case 4:
+      result[0] = [0, 3];
+      result[1] = [0, 4];
+      result[2] = [1, 4];
+      result[3] = [0, 5];
+      break;
+    case 5:
+      result[0] = [0, 4];
+      result[1] = [0, 5];
+      result[2] = [1, 4];
+      result[3] = [1, 5];
+      break;
+    case 6:
+      result[0] = [0, 3];
+      result[1] = [0, 4];
+      result[2] = [0, 5];
+      result[3] = [0, 6];
+      break;
+  }
+
+  tetroid = result;
+  tetroidType = type;
 }
